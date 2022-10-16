da727ea5787f0f35e7a58807e522c5114a0035f5
William Ashbee <wsashbee@gmail.com>
2022-10-16 18:39:00 -0400

    modified the exports file.

--- exports.sh
+++ exports.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 FSLDIR=/usr/share/fsl/5.0
-export PATH=$PATH:/usr/lib/afni/bin:/usr/lib/ROBEX:/usr/lib/ants
+PATH=$PATH:/usr/lib/afni/bin:/usr/lib/ROBEX:/usr/lib/ants
 export ANTSPATH=/usr/lib/ants
-export AFNIbinPATH = /usr/local/AFNIbin/
+export AFNIbinPATH=/usr/local/AFNIbin/
+PATH=$PATH:$AFNIbinPATH
 export FSLDIR PATH
 . ${FSLDIR}/etc/fslconf/fsl.sh
 
