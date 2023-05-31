94b711cf8bf78154d967be28b74e0f61efe12bc1
Joanne Wardell <joanne.wardell6@gmail.com>
2023-05-31 14:40:28 +0000

    Update sub_dsall master submission script to use new examples directory.

--- exports.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FSLDIR=/usr/share/fsl/5.0
-PATH=$PATH:/usr/lib/afni/bin:/usr/lib/ROBEX:/usr/lib/ants
-export ANTSPATH=/usr/lib/ants
-export AFNIbinPATH=/usr/local/AFNIbin/
-PATH=$PATH:$AFNIbinPATH
-export FSLDIR PATH
-. ${FSLDIR}/etc/fslconf/fsl.sh
-
--- sub_dsall.sh
+++ sub_dsall.sh
@@ -11,15 +11,15 @@ cd /data/users2/jwardell1/nshor_docker
 # 6. Executes script to make paths file and output directories for each subject
 # 7. Submits the dataset to slurm for processing
 #####################################################################################################gg
-cp /data/users2/jwardell1/nshor_docker/fbirn-project/FBIRN/mkpths.sh /data/users2/jwardell1/nshor_docker/fbirn-project/
-cp /data/users2/jwardell1/nshor_docker/fbirn-project/FBIRN/subjects.txt /data/users2/jwardell1/nshor_docker/fbirn-project/
-rm -rf /data/users2/jwardell1/nshor_docker/fbirn-project/FBIRN
-mkdir /data/users2/jwardell1/nshor_docker/fbirn-project/FBIRN
-mv /data/users2/jwardell1/nshor_docker/fbirn-project/subjects.txt /data/users2/jwardell1/nshor_docker/fbirn-project/FBIRN/subjects.txt
-mv /data/users2/jwardell1/nshor_docker/fbirn-project/mkpths.sh /data/users2/jwardell1/nshor_docker/fbirn-project/FBIRN/mkpths.sh
-cd fbirn-project
-bash /data/users2/jwardell1/nshor_docker/fbirn-project/FBIRN/mkpths.sh
-bash /data/users2/jwardell1/nshor_docker/fbirn-project/submit.sh
+cp /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/fbirn-project/
+cp /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN/subjects.txt /data/users2/jwardell1/nshor_docker/examples/fbirn-project/
+rm -rf /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN
+mkdir /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN
+mv /data/users2/jwardell1/nshor_docker/examples/fbirn-project/subjects.txt /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN/subjects.txt
+mv /data/users2/jwardell1/nshor_docker/examples/fbirn-project/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN/mkpths.sh
+cd examples/fbirn-project
+bash /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN/mkpths.sh
+bash /data/users2/jwardell1/nshor_docker/examples/fbirn-project/submit.sh
 echo "fbirn submitted"
 
 
@@ -34,16 +34,16 @@ cd /data/users2/jwardell1/nshor_docker
 # 6. Executes script to make paths file and output directories for each subject
 # 7. Submits the dataset to slurm for processing
 #####################################################################################################gg
-cp /data/users2/jwardell1/nshor_docker/bsnip-project/BSNIP/Boston/mkpths.sh /data/users2/jwardell1/nshor_docker/bsnip-project/
-cp /data/users2/jwardell1/nshor_docker/bsnip-project/BSNIP/Boston/subjects.txt /data/users2/jwardell1/nshor_docker/bsnip-project/
-rm -rf /data/users2/jwardell1/nshor_docker/bsnip-project/BSNIP
-mkdir /data/users2/jwardell1/nshor_docker/bsnip-project/BSNIP
-mkdir /data/users2/jwardell1/nshor_docker/bsnip-project/BSNIP/Boston
-mv /data/users2/jwardell1/nshor_docker/bsnip-project/subjects.txt /data/users2/jwardell1/nshor_docker/bsnip-project/BSNIP/Boston/subjects.txt
-mv /data/users2/jwardell1/nshor_docker/bsnip-project/mkpths.sh /data/users2/jwardell1/nshor_docker/bsnip-project/BSNIP/Boston/mkpths.sh
-cd bsnip-project
-bash /data/users2/jwardell1/nshor_docker/bsnip-project/BSNIP/Boston/mkpths.sh
-bash /data/users2/jwardell1/nshor_docker/bsnip-project/submit.sh
+cp /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/bsnip-project/
+cp /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston/subjects.txt /data/users2/jwardell1/nshor_docker/examples/bsnip-project/
+rm -rf /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP
+mkdir /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP
+mkdir /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston
+mv /data/users2/jwardell1/nshor_docker/examples/bsnip-project/subjects.txt /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston/subjects.txt
+mv /data/users2/jwardell1/nshor_docker/examples/bsnip-project/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston/mkpths.sh
+cd examples/bsnip-project
+bash /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston/mkpths.sh
+bash /data/users2/jwardell1/nshor_docker/examples/bsnip-project/submit.sh
 echo "bsnip submitted"
 
 
@@ -62,15 +62,15 @@ cd /data/users2/jwardell1/nshor_docker
 # 6. Executes script to make paths file and output directories for each subject
 # 7. Submits the dataset to slurm for processing
 #####################################################################################################gg
-cp /data/users2/jwardell1/nshor_docker/hcp-project/HCP/mkpths.sh /data/users2/jwardell1/nshor_docker/hcp-project/
-cp /data/users2/jwardell1/nshor_docker/hcp-project/HCP/subjects.txt /data/users2/jwardell1/nshor_docker/hcp-project/
-rm -rf /data/users2/jwardell1/nshor_docker/hcp-project/HCP
-mkdir /data/users2/jwardell1/nshor_docker/hcp-project/HCP
-mv /data/users2/jwardell1/nshor_docker/hcp-project/subjects.txt /data/users2/jwardell1/nshor_docker/hcp-project/HCP/subjects.txt
-mv /data/users2/jwardell1/nshor_docker/hcp-project/mkpths.sh /data/users2/jwardell1/nshor_docker/hcp-project/HCP/mkpths.sh
-cd hcp-project
-bash /data/users2/jwardell1/nshor_docker/hcp-project/HCP/mkpths.sh
-bash /data/users2/jwardell1/nshor_docker/hcp-project/submit.sh
+cp /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/hcp-project/
+cp /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP/subjects.txt /data/users2/jwardell1/nshor_docker/examples/hcp-project/
+rm -rf /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP
+mkdir /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP
+mv /data/users2/jwardell1/nshor_docker/examples/hcp-project/subjects.txt /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP/subjects.txt
+mv /data/users2/jwardell1/nshor_docker/examples/hcp-project/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP/mkpths.sh
+cd examples/hcp-project
+bash /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP/mkpths.sh
+bash /data/users2/jwardell1/nshor_docker/examples/hcp-project/submit.sh
 echo "hcp submitted"
 
 
@@ -86,13 +86,13 @@ cd /data/users2/jwardell1/nshor_docker
 # 6. Executes script to make paths file and output directories for each subject
 # 7. Submits the dataset to slurm for processing
 #####################################################################################################
-cp /data/users2/jwardell1/nshor_docker/ds004078-project/ds004078/mkpths.sh /data/users2/jwardell1/nshor_docker/ds004078-project/
-cp /data/users2/jwardell1/nshor_docker/ds004078-project/ds004078/subjects.txt /data/users2/jwardell1/nshor_docker/ds004078-project/
-rm -rf /data/users2/jwardell1/nshor_docker/ds004078-project/ds004078
-mkdir /data/users2/jwardell1/nshor_docker/ds004078-project/ds004078
-mv /data/users2/jwardell1/nshor_docker/ds004078-project/subjects.txt /data/users2/jwardell1/nshor_docker/ds004078-project/ds004078/subjects.txt
-mv /data/users2/jwardell1/nshor_docker/ds004078-project/mkpths.sh /data/users2/jwardell1/nshor_docker/ds004078-project/ds004078/mkpths.sh
-cd ds004078-project
-bash /data/users2/jwardell1/nshor_docker/ds004078-project/ds004078/mkpths.sh
-bash /data/users2/jwardell1/nshor_docker/ds004078-project/submit.sh
+cp /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/ds004078-project/
+cp /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078/subjects.txt /data/users2/jwardell1/nshor_docker/examples/ds004078-project/
+rm -rf /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078
+mkdir /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078
+mv /data/users2/jwardell1/nshor_docker/examples/ds004078-project/subjects.txt /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078/subjects.txt
+mv /data/users2/jwardell1/nshor_docker/examples/ds004078-project/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078/mkpths.sh
+cd examples/ds004078-project
+bash /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078/mkpths.sh
+bash /data/users2/jwardell1/nshor_docker/examples/ds004078-project/submit.sh
 echo "ds004078 submitted"
