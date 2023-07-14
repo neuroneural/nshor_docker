51c1442aeaaad52f6cb1ad2aac6d86f0fcde2ea9
Joanne Wardell <joanne.wardell6@gmail.com>
2023-07-14 03:38:58 +0000

    Update files for computing brain mask avg for FBIRN dataset

--- examples/fbirn-project/helper_scripts/avg.sh
+++ examples/fbirn-project/helper_scripts/avg.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-
-num_subs=`ls FBIRN/derivatives/ | wc -l`
+subs_file=${myroot}/FBIRN/subjects.txt
+myroot=/data/users2/jwardell1/nshor_docker/examples/fbirn-project
+num_subs=$(wc -l < "$subs_file")
 IFS=$'\n' sub_ids=($(cat subjects.txt))
 sum=0
 for(( i=0; i<$num_subs; i++))
 do
 	subjectID=${sub_ids[$i]}
-	if [ ! -f FBIRN/derivatives/${subjectID}/processed/benchTime.txt ]; then
-		echo "$time" >  FBIRN/derivatives/${subjectID}/processed/benchTime.txt
+	if [ ! -f ${myroot}/FBIRN/derivatives/${subjectID}/processed/benchTime.txt ]; then
+		echo "$time" >  ${myroot}/FBIRN/derivatives/${subjectID}/processed/benchTime.txt
 	fi
-	time=`cat FBIRN/derivatives/${subjectID}/processed/benchTime.txt`
+	time=`cat ${myroot}/FBIRN/derivatives/${subjectID}/processed/benchTime.txt`
 	sum=$(($sum + $time))
 done
 avg=$(($sum/$num_subs))
--- sub_dsall.sh
+++ sub_dsall.sh
@@ -1,98 +1,125 @@
 #!/bin/bash
 
-cd /data/users2/jwardell1/nshor_docker
-######################################################################################################
-# FBIRN
-# 1. Copies script to make paths file and output directory into project dir
-# 2. Copies textfile list of subjects into project dir
-# 3. Deletes existing processed files in output directory
-# 4. Makes new empty output directory 
-# 5. Moves subjects list and script for making paths file and output directories into output directory 
-# 6. Executes script to make paths file and output directories for each subject
-# 7. Submits the dataset to slurm for processing
-#####################################################################################################gg
-cp /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/fbirn-project/
-cp /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN/subjects.txt /data/users2/jwardell1/nshor_docker/examples/fbirn-project/
-rm -rf /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN
-mkdir /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN
-mv /data/users2/jwardell1/nshor_docker/examples/fbirn-project/subjects.txt /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN/subjects.txt
-mv /data/users2/jwardell1/nshor_docker/examples/fbirn-project/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN/mkpths.sh
-cd examples/fbirn-project
-bash /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN/mkpths.sh
-bash /data/users2/jwardell1/nshor_docker/examples/fbirn-project/submit.sh
-echo "fbirn submitted"
+function submit_fbirn() {
+	######################################################################################################
+	# FBIRN
+	# 1. Copies script to make paths file and output directory into project dir
+	# 2. Copies textfile list of subjects into project dir
+	# 3. Deletes existing processed files in output directory
+	# 4. Makes new empty output directory 
+	# 5. Moves subjects list and script for making paths file and output directories into output directory 
+	# 6. Executes script to make paths file and output directories for each subject
+	# 7. Submits the dataset to slurm for processing
+	#####################################################################################################gg
+	cp /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/fbirn-project/
+	cp /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN/subjects.txt /data/users2/jwardell1/nshor_docker/examples/fbirn-project/
+	rm -rf /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN
+	mkdir /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN
+	mv /data/users2/jwardell1/nshor_docker/examples/fbirn-project/subjects.txt /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN/subjects.txt
+	mv /data/users2/jwardell1/nshor_docker/examples/fbirn-project/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN/mkpths.sh
+	bash /data/users2/jwardell1/nshor_docker/examples/fbirn-project/FBIRN/mkpths.sh
+	bash /data/users2/jwardell1/nshor_docker/examples/fbirn-project/submit.sh
+	echo "fbirn submitted"
+}
 
 
-cd /data/users2/jwardell1/nshor_docker
-######################################################################################################
-# BSNIP
-# 1. Copies script to make paths file and output directory into project dir
-# 2. Copies textfile list of subjects into project dir
-# 3. Deletes existing processed files in output directory
-# 4. Makes new empty output directory 
-# 5. Moves subjects list and script for making paths file and output directories into output directory 
-# 6. Executes script to make paths file and output directories for each subject
-# 7. Submits the dataset to slurm for processing
-#####################################################################################################gg
-cp /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/bsnip-project/
-cp /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston/subjects.txt /data/users2/jwardell1/nshor_docker/examples/bsnip-project/
-rm -rf /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP
-mkdir /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP
-mkdir /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston
-mv /data/users2/jwardell1/nshor_docker/examples/bsnip-project/subjects.txt /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston/subjects.txt
-mv /data/users2/jwardell1/nshor_docker/examples/bsnip-project/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston/mkpths.sh
-cd examples/bsnip-project
-bash /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston/mkpths.sh
-bash /data/users2/jwardell1/nshor_docker/examples/bsnip-project/submit.sh
-echo "bsnip submitted"
-
-
+function submit_bsnip() {
+	######################################################################################################
+	# BSNIP
+	# 1. Copies script to make paths file and output directory into project dir
+	# 2. Copies textfile list of subjects into project dir
+	# 3. Deletes existing processed files in output directory
+	# 4. Makes new empty output directory 
+	# 5. Moves subjects list and script for making paths file and output directories into output directory 
+	# 6. Executes script to make paths file and output directories for each subject
+	# 7. Submits the dataset to slurm for processing
+	#####################################################################################################gg
+	cp /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/bsnip-project/
+	cp /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston/subjects.txt /data/users2/jwardell1/nshor_docker/examples/bsnip-project/
+	rm -rf /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP
+	mkdir /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP
+	mkdir /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston
+	mv /data/users2/jwardell1/nshor_docker/examples/bsnip-project/subjects.txt /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston/subjects.txt
+	mv /data/users2/jwardell1/nshor_docker/examples/bsnip-project/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston/mkpths.sh
+	bash /data/users2/jwardell1/nshor_docker/examples/bsnip-project/BSNIP/Boston/mkpths.sh
+	bash /data/users2/jwardell1/nshor_docker/examples/bsnip-project/submit.sh
+	echo "bsnip submitted"
+}
 
 
 
 
-cd /data/users2/jwardell1/nshor_docker
-######################################################################################################
-# HCP
-# 1. Copies script to make paths file and output directory into project dir
-# 2. Copies textfile list of subjects into project dir
-# 3. Deletes existing processed files in output directory
-# 4. Makes new empty output directory 
-# 5. Moves subjects list and script for making paths file and output directories into output directory 
-# 6. Executes script to make paths file and output directories for each subject
-# 7. Submits the dataset to slurm for processing
-#####################################################################################################gg
-cp /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/hcp-project/
-cp /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP/subjects.txt /data/users2/jwardell1/nshor_docker/examples/hcp-project/
-rm -rf /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP
-mkdir /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP
-mv /data/users2/jwardell1/nshor_docker/examples/hcp-project/subjects.txt /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP/subjects.txt
-mv /data/users2/jwardell1/nshor_docker/examples/hcp-project/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP/mkpths.sh
-cd examples/hcp-project
-bash /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP/mkpths.sh
-bash /data/users2/jwardell1/nshor_docker/examples/hcp-project/submit.sh
-echo "hcp submitted"
+function submit_hcp() {
+	######################################################################################################
+	# HCP
+	# 1. Copies script to make paths file and output directory into project dir
+	# 2. Copies textfile list of subjects into project dir
+	# 3. Deletes existing processed files in output directory
+	# 4. Makes new empty output directory 
+	# 5. Moves subjects list and script for making paths file and output directories into output directory 
+	# 6. Executes script to make paths file and output directories for each subject
+	# 7. Submits the dataset to slurm for processing
+	#####################################################################################################gg
+	cp /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/hcp-project/
+	cp /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP/subjects.txt /data/users2/jwardell1/nshor_docker/examples/hcp-project/
+	rm -rf /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP
+	mkdir /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP
+	mv /data/users2/jwardell1/nshor_docker/examples/hcp-project/subjects.txt /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP/subjects.txt
+	mv /data/users2/jwardell1/nshor_docker/examples/hcp-project/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP/mkpths.sh
+	bash /data/users2/jwardell1/nshor_docker/examples/hcp-project/HCP/mkpths.sh
+	bash /data/users2/jwardell1/nshor_docker/examples/hcp-project/submit.sh
+	echo "hcp submitted"
+}
+
+
+function submit_hcpnm() {
+	######################################################################################################
+	# HCP Neuromark
+	# 1. Copies script to make paths file and output directory into project dir
+	# 2. Copies textfile list of subjects into project dir
+	# 3. Deletes existing processed files in output directory
+	# 4. Makes new empty output directory 
+	# 5. Moves subjects list and script for making paths file and output directories into output directory 
+	# 6. Executes script to make paths file and output directories for each subject
+	# 7. Submits the dataset to slurm for processing
+	#####################################################################################################gg
+	cp /data/users2/jwardell1/nshor_docker/examples/hcpnm-project/HCP/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/hcpnm-project/
+	cp /data/users2/jwardell1/nshor_docker/examples/hcpnm-project/HCP/subjects.txt /data/users2/jwardell1/nshor_docker/examples/hcpnm-project/
+	rm -rf /data/users2/jwardell1/nshor_docker/examples/hcpnm-project/HCP
+	mkdir /data/users2/jwardell1/nshor_docker/examples/hcpnm-project/HCP
+	mv /data/users2/jwardell1/nshor_docker/examples/hcpnm-project/subjects.txt /data/users2/jwardell1/nshor_docker/examples/hcpnm-project/HCP/subjects.txt
+	mv /data/users2/jwardell1/nshor_docker/examples/hcpnm-project/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/hcpnm-project/HCP/mkpths.sh
+	bash /data/users2/jwardell1/nshor_docker/examples/hcpnm-project/HCP/mkpths.sh
+	bash /data/users2/jwardell1/nshor_docker/examples/hcpnm-project/submit.sh
+	echo "hcp neuromark submitted"
+}
 
 
 
-cd /data/users2/jwardell1/nshor_docker
-######################################################################################################
-# ds004078
-# 1. Copies script to make paths file and output directory into project dir
-# 2. Copies textfile list of subjects into project dir
-# 3. Deletes existing processed files in output directory
-# 4. Makes new empty output directory 
-# 5. Moves subjects list and script for making paths file and output directories into output directory 
-# 6. Executes script to make paths file and output directories for each subject
-# 7. Submits the dataset to slurm for processing
-#####################################################################################################
-cp /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/ds004078-project/
-cp /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078/subjects.txt /data/users2/jwardell1/nshor_docker/examples/ds004078-project/
-rm -rf /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078
-mkdir /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078
-mv /data/users2/jwardell1/nshor_docker/examples/ds004078-project/subjects.txt /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078/subjects.txt
-mv /data/users2/jwardell1/nshor_docker/examples/ds004078-project/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078/mkpths.sh
-cd examples/ds004078-project
-bash /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078/mkpths.sh
-bash /data/users2/jwardell1/nshor_docker/examples/ds004078-project/submit.sh
-echo "ds004078 submitted"
+function submit_ds004078() {
+	######################################################################################################
+	# ds004078
+	# 1. Copies script to make paths file and output directory into project dir
+	# 2. Copies textfile list of subjects into project dir
+	# 3. Deletes existing processed files in output directory
+	# 4. Makes new empty output directory 
+	# 5. Moves subjects list and script for making paths file and output directories into output directory 
+	# 6. Executes script to make paths file and output directories for each subject
+	# 7. Submits the dataset to slurm for processing
+	#####################################################################################################
+	cp /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/ds004078-project/
+	cp /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078/subjects.txt /data/users2/jwardell1/nshor_docker/examples/ds004078-project/
+	rm -rf /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078
+	mkdir /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078
+	mv /data/users2/jwardell1/nshor_docker/examples/ds004078-project/subjects.txt /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078/subjects.txt
+	mv /data/users2/jwardell1/nshor_docker/examples/ds004078-project/mkpths.sh /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078/mkpths.sh
+	bash /data/users2/jwardell1/nshor_docker/examples/ds004078-project/ds004078/mkpths.sh
+	bash /data/users2/jwardell1/nshor_docker/examples/ds004078-project/submit.sh
+	echo "ds004078 submitted"
+}
+
+submit_fbirn &
+submit_bsnip &
+submit_hcp &
+submit_hcpnm &
+submit_ds004078 &
