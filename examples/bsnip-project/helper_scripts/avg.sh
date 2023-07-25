da4f387da2bb94513fb24d435576f213cddc8811
Joanne Wardell <joanne.wardell6@gmail.com>
2023-07-25 16:53:51 +0000

    Add files for HCP group mean mask calculation, continue processing missing HCP subs, delete unused scripts for bsnip, hcp, and fbirn.

--- examples/bsnip-project/helper_scripts/avg.sh
+++ examples/bsnip-project/helper_scripts/avg.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+project_dir=/data/users2/jwardell1/nshor_docker/examples/bsnip-project
 
-num_subs=`ls FBIRN/derivatives/ | wc -l`
-IFS=$'\n' sub_ids=($(cat subjects.txt))
-sum=0
+subs_file=${project_dir}/BSNIP/Boston/subjects.txt
+num_subs=$(cat $subs_file | wc -l)
+
+IFS=$'\n' sub_ids=($(cat $subs_file))
+
+speeds_file=${project_dir}/speeds.txt
+
+if [ -f $speeds_file ]; then
+	rm $speeds_file
+else
+	touch $speeds_file
+fi
+
 for(( i=0; i<$num_subs; i++))
 do
 	subjectID=${sub_ids[$i]}
-	if [ ! -f FBIRN/derivatives/${subjectID}/processed/benchTime.txt ]; then
-		echo "$time" >  FBIRN/derivatives/${subjectID}/processed/benchTime.txt
+	if [ ! -f ${project_dir}/BSNIP/Boston/${subjectID}/ses_01/processed/benchTime.txt ]; then
+		continue
 	fi
-	time=`cat FBIRN/derivatives/${subjectID}/processed/benchTime.txt`
-	sum=$(($sum + $time))
+	time=`cat ${project_dir}/BSNIP/Boston/${subjectID}/ses_01/processed/benchTime.txt`
+	echo $time >> $speeds_file
 done
-avg=$(($sum/$num_subs))
-echo "$avg" > avg.txt
