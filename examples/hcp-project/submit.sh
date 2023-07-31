c4aec09b9d7b8c58597888b87eac0885e2f7b929
Joanne Wardell <joanne.wardell6@gmail.com>
2023-07-31 19:22:18 +0000

    update slurm jobs to print unique logs for each parallel run

--- examples/hcp-project/submit.sh
+++ examples/hcp-project/submit.sh
@@ -5,6 +5,7 @@ project_dir=/data/users2/jwardell1/nshor_docker/examples/hcp-project
 paths_file=${project_dir}/HCP/paths
 
 num_lines=`wc -l <  $paths_file`
-num_total_runs=$(( $num_lines / 9  ))
+num_total_runs=$(( $num_lines / 10 ))
+runix=$(( $num_total_runs - 1 ))
 
-sbatch --array=0-$num_total_runs ${project_dir}/procruns.job
+sbatch --array=0-${runix}%10 ${project_dir}/procruns.job
