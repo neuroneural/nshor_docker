e6900d3442ce5021da5c9096d5481a043b5b79c1
Joanne Wardell <joanne.wardell6@gmail.com>
2023-12-15 17:03:21 +0000

    Add files for computing fnc

--- examples/bsnip-project/helper_scripts/compute_fnc.py
+++ examples/bsnip-project/helper_scripts/compute_fnc.py
@@ -3,12 +3,14 @@ import numpy as np
 from scipy.io import loadmat
 from scipy.stats import zscore
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 
-def compute_fnc(subject_id, timecourse_file, output_dir, downsampling_factor):
+def compute_fnc(subject_id, timecourse_file, output_dir):
     try:
         print(f'subject id {subject_id}')
         print(f'timecourses file {timecourse_file}')
         print(f'output dir {output_dir}')
+        
         # Load the timecourse file for the current subject
         timecourse_data = loadmat(timecourse_file)
         timecourses = timecourse_data['TCMax']  # Assuming 'TCMax' is the variable name in the .mat file
@@ -24,24 +26,20 @@ def compute_fnc(subject_id, timecourse_file, output_dir, downsampling_factor):
         # Compute FNC matrix (correlation matrix of timecourses)
         fnc_matrix = np.corrcoef(zscored_timecourses)
 
-	# Apply downsampling
-        if downsampling_factor > 1:
-            timecourses = timecourses[:, ::downsampling_factor]  # Downsample by taking every nth sample
-            print(f"apply downsampling {downsampling_factor}")
+        # Set diagonal elements to zero
+        np.fill_diagonal(fnc_matrix, 0)
 
+        # Create a custom red-white-blue colormap
+        cmap_colors = [(0, 'blue'), (0.5, 'white'), (1, 'red')]
+        custom_cmap = LinearSegmentedColormap.from_list('custom_RWB', cmap_colors)
 
-        # Save FNC matrix to a file
-        if downsampling_factor > 1:
-            output_file = f'{output_dir}/FNC_{subject_id}_ds_{downsampling_factor}.npy'
-        else:
-            output_file = f'{output_dir}/FNC_{subject_id}.npy'
-        
-        np.save(output_file, fnc_matrix)
+        # Plot FNC matrix using the custom colormap
+        plt.imshow(fnc_matrix, cmap=custom_cmap, vmin=-1, vmax=1)
+        plt.colorbar()  # Add color bar for reference
 
-        plt.imshow(fnc_matrix)
-        savename = '{}/{}_ds_{}_fnc.png'.format(output_dir, subject_id, downsampling_factor)
+        savename = '{}/{}_fnc.png'.format(output_dir, subject_id)
         plt.savefig(savename)
-        
+
         print(f"FNC matrix computed and saved for subject {subject_id}")
 
     except FileNotFoundError:
@@ -50,12 +48,10 @@ def compute_fnc(subject_id, timecourse_file, output_dir, downsampling_factor):
         print(f"Could not find 'TCMax' variable in the .mat file for subject {subject_id}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python script.py <subject_id> <timecourse_file> <output_dir> <downsampling_factor>")
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <subject_id> <timecourse_file> <output_dir>")
     else:
         subject_id = sys.argv[1]
         timecourse_file = sys.argv[2]
         output_dir = sys.argv[3]
-        downsampling_factor = int(sys.argv[4])
-        compute_fnc(subject_id, timecourse_file, output_dir, downsampling_factor)
-
+        compute_fnc(subject_id, timecourse_file, output_dir)
