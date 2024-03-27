9227c093fa00f3a1930ec12fe5bb75e3773e1b3d
Joanne Wardell <joanne.wardell6@gmail.com>
2024-03-27 05:06:29 +0000

    add files for oulu data

--- examples/oulu-project/tr100/submit_mask.sh
+++ examples/oulu-project/tr100/submit_mask.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 project_dir=/data/users2/jwardell1/nshor_docker/examples/oulu-project
-paths_file=${project_dir}/tr100/paths
+paths_file=${project_dir}/tr100/subjects.txt
 
 num_lines=`wc -l <  $paths_file`
-num_total_runs=$(( $num_lines / 3 ))
+num_total_runs=$num_lines
 
 startix=0
 endix=$(( $num_total_runs - 1 ))
 
-sbatch --array=${startix}-${endix} ${project_dir}/tr100/fsl.job
+sbatch --array=${startix}-${endix} ${project_dir}/tr100/mask.job
